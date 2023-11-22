d330d85f723e334370daed53191b3ae193117b97
Felipe Vieira <flp.vieira92@gmail.com>
2023-11-22 17:31:13 -0300

    final do dia 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './componentes/Header';
 import styled from 'styled-components';
 import Pesquisa from './componentes/Pesquisa';
+import UltimosLancamentos from './componentes/UltimosLancamentos';
 
 
 
@@ -17,6 +18,7 @@ function App() {
     <AppContainer>
       <Header />
       <Pesquisa />
+      <UltimosLancamentos />
     </AppContainer>
   );
 }
---
+++ src/componentes/UltimosLancamentos/index.js
@@ -0,0 +1,37 @@
+import { livros } from "./dadosUltimosLancamentos"
+import styled from "styled-components"
+import { Titulo } from "../Titulo"
+
+const UltimosLancamentosContainer = styled.section`
+
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+
+`
+
+
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
+function UltimosLancamentos(){
+    return(
+        <UltimosLancamentosContainer>
+            <Titulo>ULTIMOS LANCAMENTOS</Titulo>
+            <NovosLivrosContainer>
+                { livros.map ( livro => (
+                    <img src={livro.src} alt="livros"/>
+                ))}
+            </NovosLivrosContainer>    
+        </UltimosLancamentosContainer>
+    )
+}
+
+export default UltimosLancamentos
